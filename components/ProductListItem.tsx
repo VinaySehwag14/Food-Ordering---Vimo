8c7848f3dd1ace381c35a9a96c00c3a200b9d290
VinaySehwag14 <vinaysehwag14@gmail.com>
2024-03-11 13:45:23 +0530

    make separate component and fixed type issue

---
+++ components/ProductListItem.tsx
@@ -0,0 +1,51 @@
+import Colors from "@/constants/Colors";
+import { Product } from "@/src/types";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={{
+          uri:
+            product.image ??
+            "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png",
+        }}
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+  },
+  image: {
+    width: `100%`,
+    aspectRatio: 1,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+  },
+  price: {
+    color: Colors.light.tint,
+  },
+});
--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,39 +1,12 @@
 import products from "@/assets/data/products";
-import Colors from "@/constants/Colors";
-import { Image, StyleSheet, Text, View } from "react-native";
+import ProductListItem from "@/components/ProductListItem";
+import { View } from "react-native";
 
-export default function TabOneScreen() {
-  const product = products[0];
+export default function MenuScreen() {
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: product.image }} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <ProductListItem product={products[0]} />
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white",
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: `100%`,
-    aspectRatio: 1,
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: "600",
-    marginVertical: 10,
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-  price: {
-    color: Colors.light.tint,
-  },
-});
